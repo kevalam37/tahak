59eea8b2860481c9255b5c638017c5ef949aea6a
Václav Bejtler <bejtler@seznam.cz>
2025-02-22 21:21:38 +0100

    naplněnní článků lorem ipsum

--- clanky.js
+++ clanky.js
@@ -1,41 +1,41 @@
 const clanky = [
     {
         nadpis: "Jak začít s programováním?",
-        obsah: "Programování je skvělá dovednost, která ti umožní tvořit aplikace, weby a další projekty.",
+        obsah: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         id: "programovani",
         kod: `console.log('Ahoj světe!');`,
-        popisek: "Tento kód zobrazí v konzoli text 'Ahoj světe!'.",
+        popisek: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         images: ["images/01.jpg", "images/02.jpg"],
     },
     {
         nadpis: "Proč používat GitHub Pages?",
-        obsah: "GitHub Pages je skvělý způsob, jak zdarma hostovat statické webové stránky.",
+        obsah: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         id: "github-pages",
         kod: `<h1>Hello GitHub Pages</h1>`,
-        popisek: "Jednoduchý příklad HTML kódu, který zobrazuje nadpis.",
+        popisek: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         images: ["images/03.jpg", "images/04.jpg"],
     },
     {
         nadpis: "h2",
-        obsah: "V HTML můžeš vytvořit pomocí tagu h2 nenší nadpis.",
+        obsah: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         id: "h2",
         kod: `<h2>Nadpis h2</h2>`,
-        popisek: "Tento kód přepíše obsah stránky a zobrazí nadpis 'Nadpis h2'.",
+        popisek: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         
     },
     {
         nadpis: "h3",
-        obsah: "Dynamické články v HTML můžeš vytvořit pomocí JavaScriptu a pole objektů.",
+        obsah: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         id: "h3",
         kod: `document.body.innerHTML = "<h1>Dynamický obsah</h1>";`,
-        popisek: "Tento kód přepíše obsah stránky a zobrazí nadpis 'h3'.",
+        popisek: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         images: ["images/05.jpg", "images/06.jpg"],
     },{
         nadpis: "Jak vytvořit dynamické články?",
-        obsah: "Dynamické články v HTML můžeš vytvořit pomocí JavaScriptu a pole objektů.",
+        obsah: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         id: "dynamicke-clanky",
         kod: `document.body.innerHTML = "<h1>Dynamický obsah</h1>";`,
-        popisek: "Tento kód přepíše obsah stránky a zobrazí nadpis 'Dynamický obsah'.",
+        popisek: "„Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         images: ["images/07.jpg"],
     }
 
